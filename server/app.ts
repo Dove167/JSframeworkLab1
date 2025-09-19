59dba6a4ace2c4bd2c11c228226167a928020fd2
Dove167 <jfajardo7@my.bcit.ca>
2025-09-19 14:42:34 -0700

    Lab 6: Tanstack Query integration with API

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,21 @@
 // server/app.ts
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
+import { cors } from 'hono/cors'
 import { expensesRoute } from './routes/expenses'
 
 export const app = new Hono()
 
 // Global logger (from Lab 1)
 app.use('*', logger())
+
+// CORS middleware
+app.use('*', cors({
+  origin: 'http://localhost:5173',
+  allowMethods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}))
 
 // Custom timing middleware
 app.use('*', async (c, next) => {
